241b8515be0d5dd1e64e384bfb13db6109020e18
Peter <46471156+fortierpm@users.noreply.github.com>
2021-01-24 17:55:20 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function parallax(element, distance, speed) {
+    const item = document.querySelector(element);
+    item.style.transform = `translateY(${distance * speed}px)`;
+}
+
+window.addEventListener("scroll", () => {
+    parallax(".big-rose", window.scrollY, 0.2);
+    parallax(".small-rose", window.scrollY, 0.4);
+});
